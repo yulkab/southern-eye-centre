0a97cebf2f7fd479cf8311869fd68ebb1671cad0
Yulia <yulkab@gmail.com>
2019-09-21 13:42:26 +1000

    Updates

---
+++ precache-manifest.69ae0d1cd387f3cd32d1eeb8aed05861.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b832c51d9a67359bbba429c2692040",
+    "url": "/southern-eye-centre/index.html"
+  },
+  {
+    "revision": "e6b83be84f94ecfd9be5",
+    "url": "/southern-eye-centre/static/css/main.ac0d6b62.chunk.css"
+  },
+  {
+    "revision": "6c495caf557a0b674b77",
+    "url": "/southern-eye-centre/static/js/2.8327565f.chunk.js"
+  },
+  {
+    "revision": "1512f66183b050195260",
+    "url": "/southern-eye-centre/static/js/3.5b38a94c.chunk.js"
+  },
+  {
+    "revision": "e6b83be84f94ecfd9be5",
+    "url": "/southern-eye-centre/static/js/main.2dd33569.chunk.js"
+  },
+  {
+    "revision": "e95e012990cc47027252",
+    "url": "/southern-eye-centre/static/js/runtime~main.b462439b.js"
+  },
+  {
+    "revision": "7688a68882136ee03424d2f722b3e0dd",
+    "url": "/southern-eye-centre/static/media/1.7688a688.jpg"
+  },
+  {
+    "revision": "2ac0df04df080c6cc5e4e6a82aa3ff5e",
+    "url": "/southern-eye-centre/static/media/2.2ac0df04.jpg"
+  },
+  {
+    "revision": "404a55fb1f8e8913905c686025a9bf5f",
+    "url": "/southern-eye-centre/static/media/allergies-image2.404a55fb.png"
+  },
+  {
+    "revision": "15fec029fff5e366db6e3a08b8db6a45",
+    "url": "/southern-eye-centre/static/media/cataract-anatomy.15fec029.png"
+  },
+  {
+    "revision": "99f8428dcaa416d5c2bfca387815ed88",
+    "url": "/southern-eye-centre/static/media/cataract-anatomy@2x.99f8428d.png"
+  },
+  {
+    "revision": "7a7376dd18e6647d608481f1fbdb08a7",
+    "url": "/southern-eye-centre/static/media/centre-1.7a7376dd.jpg"
+  },
+  {
+    "revision": "8d5e0b7f0f0237dd7d7faac0363029b0",
+    "url": "/southern-eye-centre/static/media/doctor-photo@2x.8d5e0b7f.png"
+  },
+  {
+    "revision": "66fc78072d7a1e49d414fe06b2cbf723",
+    "url": "/southern-eye-centre/static/media/eye-icon-small.66fc7807.png"
+  },
+  {
+    "revision": "f6a73e5505848b9314582fa501361cc7",
+    "url": "/southern-eye-centre/static/media/eye-icon-small@2x.f6a73e55.png"
+  },
+  {
+    "revision": "da370fa3b48f9e7e7cc197c2efb7e909",
+    "url": "/southern-eye-centre/static/media/eye-icon-small@3x.da370fa3.png"
+  },
+  {
+    "revision": "c56e102b85fcd0c467b2e5e3bba53b75",
+    "url": "/southern-eye-centre/static/media/eye-icon-xl@2x.c56e102b.png"
+  },
+  {
+    "revision": "17ef9d710b5535d20da28010ba023317",
+    "url": "/southern-eye-centre/static/media/family-image@2x.17ef9d71.png"
+  },
+  {
+    "revision": "26058d3c92bfec8d793b8171729659d8",
+    "url": "/southern-eye-centre/static/media/home-side.26058d3c.jpg"
+  },
+  {
+    "revision": "1d7db244ee5c3de2e9c9547b9083eb7d",
+    "url": "/southern-eye-centre/static/media/illustration-home-1m.1d7db244.png"
+  },
+  {
+    "revision": "d7328fb49595c30333d35d1399298b89",
+    "url": "/southern-eye-centre/static/media/illustration-home-2m.d7328fb4.png"
+  },
+  {
+    "revision": "cb90efdf4e3b34d3606b0a049037aa5d",
+    "url": "/southern-eye-centre/static/media/illustration-home-3m.cb90efdf.png"
+  },
+  {
+    "revision": "0f9fc544c5c10f2e5218e7df26a701f8",
+    "url": "/southern-eye-centre/static/media/line-bg@2x.0f9fc544.png"
+  },
+  {
+    "revision": "46059a6ca2369f87c6637cf77ee55572",
+    "url": "/southern-eye-centre/static/media/map.46059a6c.jpg"
+  },
+  {
+    "revision": "e90570924895291a08e4337c83ebb5d2",
+    "url": "/southern-eye-centre/static/media/personal-image2.e9057092.png"
+  },
+  {
+    "revision": "f796b552ea27e6b74bd05efe3bd865df",
+    "url": "/southern-eye-centre/static/media/referral-image@2x.f796b552.png"
+  },
+  {
+    "revision": "0f4f27ccb7fede1978ba2f64e67178a6",
+    "url": "/southern-eye-centre/static/media/registration-image@2x.0f4f27cc.png"
+  },
+  {
+    "revision": "931e2ebd5329de0d280101615161464b",
+    "url": "/southern-eye-centre/static/media/staff.931e2ebd.jpg"
+  },
+  {
+    "revision": "badb6278e101fa7ff738b6ba2d77ff67",
+    "url": "/southern-eye-centre/static/media/stam.badb6278.jpg"
+  },
+  {
+    "revision": "a2ecf2d472563f15b90a2f3c2b05fd77",
+    "url": "/southern-eye-centre/static/media/surgery@2x.a2ecf2d4.png"
+  }
+]);
